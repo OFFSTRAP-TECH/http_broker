7a9ae5000496ad0ea978f58bdf6b19149850a50d
Aryan Sardana <aryansardana1@gmail.com>
2025-02-13 13:01:04 +0530

    happy

---
+++ app.js
@@ -0,0 +1,44 @@
+import express from "express"
+import bodyParser from "body-parser"
+import { MongoClient } from 'mongodb';
+
+//offstrap_admin: offstrap_admin
+const app=express()
+const uri = "mongodb+srv://offstrap_admin:offstrap_admin@offstrapprototype.sdgbo.mongodb.net/?retryWrites=true&w=majority&appName=offstrapPrototype";
+const client = new MongoClient(uri);
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port=8000;
+
+try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    
+
+  } catch (error) {
+    console.error("Connection error:", error);
+  }
+
+app.get("/",(req,res)=>{
+
+    
+    res.send("Hello");
+})
+
+app.post("/prototype/1",async (req,res,next)=>{
+    
+
+        const db = client.db("testDB");
+        const collection = db.collection("users");
+        const jsonData = req.body;
+        const result = await collection.insertOne(jsonData);
+        console.log("Inserted");
+        console.log(result);
+        res.send(200);
+})
+app.listen(port,()=>{
+    console.log(`server live at ${port}`)
+})
